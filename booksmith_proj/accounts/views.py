647d7d34649bd313c975682412efbb53f799254c
Lance Antor <151009102+LanceAntor@users.noreply.github.com>
2024-09-23 13:39:40 +0800

    SignupUI

--- booksmith_proj/accounts/views.py
+++ booksmith_proj/accounts/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "accounts/index.html")
+
+def signup(request):
+    return render(request, "accounts/signup.html")

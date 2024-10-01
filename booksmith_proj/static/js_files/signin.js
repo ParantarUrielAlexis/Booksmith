e7440ded5e7e2696b29a79b15c7b23a53db6457f
Lance Antor <151009102+LanceAntor@users.noreply.github.com>
2024-10-01 19:25:50 +0800

    fix show password functionality

--- booksmith_proj/static/js_files/signin.js
+++ booksmith_proj/static/js_files/signin.js
@@ -1,20 +1,3 @@
-<script>
-{% if messages %}
-{% for message in messages %}
-    Swal.fire({
-        icon: '{% if message.tags == "success" %}success{% elif message.tags == "error" %}error{% else %}info{% endif %}',
-        title: '{{ message }}',
-        showConfirmButton: true,
-        timer: 3000
-    }).then(() => {
-        {% if message.tags == "success" %}
-            window.location.href = "{% url 'home' %}";
-        {% elif message.tags == "error" %}
-        {% endif %}
-    });
-{% endfor %}
-{% endif %}
-</script>
 const togglePassword = document.querySelector('#togglePassword');
 const password = document.querySelector('#pass1');
 

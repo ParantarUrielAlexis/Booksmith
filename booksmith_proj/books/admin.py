a27c2fc3ca9cc1a1ed88c6ff72c33abe2d5426d4
Uriel <leirualexisparantar@gmail.com>
2024-11-20 12:35:47 +0800

    fixed logout button on base.html

--- booksmith_proj/books/admin.py
+++ booksmith_proj/books/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Book, Author, Category
+from .models import Book, Author, Category, Review
 # Register your models here.
 admin.site.register(Book)
 admin.site.register(Author)
 admin.site.register(Category)
+admin.site.register(Review)
